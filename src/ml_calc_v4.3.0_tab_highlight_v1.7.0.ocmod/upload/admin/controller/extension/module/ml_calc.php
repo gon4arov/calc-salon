9f2d9cef12d05cd6b0805ac65313fe79c33c0db4
Sago Mac <pomprok22@gmail.com>
2025-12-14 20:30:34 +0200

    Embed email block inside results; bump to 1.7.9

--- src/ml_calc_v4.3.0_tab_highlight_v1.7.0.ocmod/upload/admin/controller/extension/module/ml_calc.php
+++ src/ml_calc_v4.3.0_tab_highlight_v1.7.0.ocmod/upload/admin/controller/extension/module/ml_calc.php
@@ -2,7 +2,7 @@
 class ControllerExtensionModuleMlCalc extends Controller {
     private $error = array();
 
-    const VERSION = '1.7.8';
+    const VERSION = '1.7.9';
 
     public function index() {
         $this->load->language('extension/module/ml_calc');
